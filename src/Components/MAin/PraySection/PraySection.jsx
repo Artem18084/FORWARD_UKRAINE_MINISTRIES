812ffc85c1479d271383678684d95b5858d0a9e3
Artem Kosharnyi <artemkkosharnyi@gmali.com>
2024-01-28 19:06:16 +0200

    pray section full respons first

--- src/Components/MAin/PraySection/PraySection.jsx
+++ src/Components/MAin/PraySection/PraySection.jsx
@@ -5,14 +5,14 @@ import DonateButton from "../../UI/DonateButton/DonateButton";
 export default function PraySection() {
   return (
     <article
-      className=" flex flex-col gap-11 items-center px-4 py-12 font-inter text-center text-[18px] font-normal leading-[27px ] tracking-normal text-white"
+      className=" flex flex-col gap-11 md:gap-[60px] items-center px-4 md:px-9 lg:px-11 xl:px-[72px] py-12 md:py-[72px] lg:py-24 font-inter text-center text-[18px] font-normal leading-[27px ] tracking-normal text-white"
       style={{ backgroundImage: `url(${prayBanner})` }}
     >
       <section className=" flex flex-col  items-center gap-2">
         <Title title="Pray for ukraine" color="white" />
         <div className="max-w-[103.5px] w-full h-[1px] bg-myYellow "></div>
       </section>
-      <p className="">
+      <p className="w-full lg:max-w-[789px] ">
         Your help is needed in Ukraine where there are already more than 2.9
         million people in need of assistance. There is great risk of additional
         suffering both within Ukraine and for those who may flee to neighboring
